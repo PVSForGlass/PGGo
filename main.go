d13e05e931ec45491dccb127d13543e18afa3819
Matthew Dawson <matthew@mjdsystems.ca>
2014-09-29 12:45:34 -0400

    Add the start of the server side.
    Add a new context function.  It doesn't do more then that.

--- main.go
+++ main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/gosexy/redis"
 )
 
 var (
 	endpoint = flag.String("e", "unix://var/run/docker.sock", "Dockerd endpoint")
-	addr = flag.String("p", ":"+os.Getenv("PORT"), "Address and port to serve dockerui")
+	redisip = flag.String("r", os.Getenv("REDISIP"), "Redis ip to connect to")
+	redisport = flag.Int("s", 6379, "Redis port to connect to")
+	addr = flag.String("p", os.Getenv("PORT"), "Address and port to serve dockerui")
 	assets = flag.String("a", ".", "Path to the assets")
 )
 
+var con *redis.Client = redis.New()
+
+func CreateContext(w http.ResponseWriter, r *http.Request) {
+	contextbytes := make([]byte, 9)
+	if _, err := rand.Read(contextbytes); err != nil {
+		log.Panic(err)
+	}
+	contextname := base64.URLEncoding.EncodeToString(contextbytes)
+	con.SAdd("VMs", contextname)
+	w.Write([]byte(contextname))
+}
+
 func main() {
 	flag.Parse()
-	if err := http.ListenAndServe(*addr, nil); err != nil {
+	
+	if err := con.Connect(*redisip, uint(*redisport)); err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/createcontext", CreateContext)
+	
+	if err := http.ListenAndServe(":" + *addr, nil); err != nil {
 		log.Fatal(err)
 	}
 }
