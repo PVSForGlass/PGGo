2fa2051c966b6b340665a95cd4f347b885d51f1a
Matthew Dawson <matthew@mjdsystems.ca>
2014-09-30 15:02:36 -0400

    Add file upload to redis.
    Upload files to redis, so things are getting ready to rumble!

--- main.go
+++ main.go
@@ -4,21 +4,26 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 	"flag"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"strconv"
 	"strings"
-	"os"
 
 	"github.com/gosexy/redis"
 )
 
 var (
-	endpoint = flag.String("e", "unix://var/run/docker.sock", "Dockerd endpoint")
+	endpoint    = flag.String("e", "unix://var/run/docker.sock", "Dockerd endpoint")
 	redisurlstr = flag.String("r", os.Getenv("REDIS_URL"), "Redis url to connect to")
-	addr = flag.String("p", os.Getenv("PORT"), "Address and port to serve dockerui")
-	assets = flag.String("a", ".", "Path to the assets")
+	addr        = flag.String("p", os.Getenv("PORT"), "Address and port to serve dockerui")
+	assets      = flag.String("a", ".", "Path to the assets")
+)
+
+var (
+	VMsKey = "VMs"
 )
 
 var con *redis.Client = redis.New()
@@ -29,13 +34,41 @@ func CreateContext(w http.ResponseWriter, r *http.Request) {
 		log.Panic(err)
 	}
 	contextname := base64.URLEncoding.EncodeToString(contextbytes)
-	con.SAdd("VMs", contextname)
+	con.SAdd(VMsKey, contextname)
 	w.Write([]byte(contextname))
+}
+
+func UploadFile(w http.ResponseWriter, r *http.Request) {
+	pathSplit := strings.Split(r.URL.Path, "/")
+	if len(pathSplit) != 4 {
+		http.NotFound(w, r)
+		return
+	}
+	context := pathSplit[2]
+	file := pathSplit[3]
+
+	if exists, err := con.SIsMember(VMsKey, context); err != nil {
+		log.Panic(err)
+	} else if !exists {
+		http.NotFound(w, r)
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	if _, err := con.HSet(context, file, string(body)); err != nil {
+		log.Panic(err)
+	}
+
+	w.Write([]byte("Success!"))
 }
 
 func main() {
 	flag.Parse()
-	
+
 	redisurl, err := url.Parse(*redisurlstr)
 	if err != nil {
 		log.Fatal(err)
@@ -50,8 +83,9 @@ func main() {
 	}
 
 	http.HandleFunc("/createcontext", CreateContext)
-	
-	if err := http.ListenAndServe(":" + *addr, nil); err != nil {
+	http.HandleFunc("/uploadfile/", UploadFile)
+
+	if err := http.ListenAndServe(":"+*addr, nil); err != nil {
 		log.Fatal(err)
 	}
 }
